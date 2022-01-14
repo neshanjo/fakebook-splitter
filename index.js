bb84d2110ea5d3835fe008016e23a29a9e74a8e0
Johannes Schneider <java@neshanjo.de>
2022-01-14 16:13:31 +0100

    added handling of empty end index and added optional offset parameter

--- index.js
+++ index.js
@@ -3,15 +3,16 @@ import { parse } from 'csv-parse/sync';
 import { execSync } from 'child_process';
 
 if (process.argv.length < 4) {
-  console.error("Usage: node index.js CSV-FILE PDF-FILE OUTPUT-DIRECTORY");
+  console.error("Usage: node index.js CSV-FILE PDF-FILE OUTPUT-DIRECTORY [OFFSET]");
   process.exit(1);
 }
 
 const csvInput = process.argv[2];
 const pdfInput = process.argv[3];
 const outputDir = process.argv[4];
+const offset = process.argv[5] ? parseInt(process.argv[5]) : 0;
 
-const songs = parse(fs.readFileSync(csvInput, "utf-8"), { 
+const songs = parse(fs.readFileSync(csvInput, "utf-8"), {
   delimiter: ",",
   columns: ["title", "begin", "end"],
   skip_empty_lines: true
@@ -23,5 +24,8 @@ for (let song of songs) {
     console.log("Skipped.");
     continue;
   }
-  execSync(`pdftk "${pdfInput}" cat ${song.begin}-${song.end} output "${outputDir}/${song.title.replace(":", "_").replace("/", "_").replace("?", "_")}.pdf"`);
+  const begin = parseInt(song.begin) + offset;
+  const end = song.end ? (parseInt(song.end) + offset) : begin;
+  console.log(`  + extracting from page ${begin} to ${end}`);
+  execSync(`pdftk "${pdfInput}" cat ${begin}-${end} output "${outputDir}/${song.title.replace(":", "_").replace("/", "_").replace("?", "_")}.pdf"`);
 }
